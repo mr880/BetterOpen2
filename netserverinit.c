23bfee2ee0d8728ec6483537e624066193aba2d1
Michael Russo <imichaelrusso@gmail.com>
2018-08-28 20:07:25 -0400

    first commit

---
+++ netserverinit.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+	
+	
+
+
+	
+	return 1;
+
+}
