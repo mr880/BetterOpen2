c148246456ab7ed91d57447fb4c55d96fa90a396
Michael Russo <imichaelrusso@gmail.com>
2018-08-29 06:36:50 -0400

    set up functions

--- netserverinit.c
+++ netserverinit.c
@@ -5,13 +5,40 @@
 #include <netinet/in.h>
 #include <string.h>
 
+int netserverinit(char * hostname, int filemode) 
+{
+
+	return 1;
+}
+
+int netclose(int fd)
+{
+
+	return 1;
+}
+
+ssize_t netwrite(int fildes, const void *buf, size_t nbyte)
+{
+
+	return 1;
+}
+
+ssize_t netread(int fildes, void *buf, size_t nbyte)
+{
+
+	return 1;
+}
+
+int netopen(const char *pathname, int flags)
+{
+
+	return 1;
+}
+
 int main(int argc, char const *argv[])
 {
-	
-	
 
 
-	
 	return 1;
 
 }
